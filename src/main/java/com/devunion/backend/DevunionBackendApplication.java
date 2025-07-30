5070610000fc288d37004e3c2f184ca72d9597cf
jisub <151591300+jisub-dev@users.noreply.github.com>
2025-07-30 16:14:34 +0900

    init files

---
+++ src/main/java/com/devunion/backend/DevunionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devunion.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevunionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevunionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devunion/backend/DevunionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devunion.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevunionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
